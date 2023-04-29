c382ac34eab23b543805df66b514346a66e7027e
typhoonw7 <wychen@clarku.edu>
2023-04-29 17:19:17 -0400

    original

--- Anagram.java
+++ Anagram.java
@@ -2,15 +2,13 @@ import java.util.HashMap;
 
 public class Anagram {
     public static boolean anagram(String oneS, String twoS) {
-        HashMap<Character, Integer> oneHM = new HashMap<Character, Integer>();
-        HashMap<Character, Integer> twoHM = new HashMap<Character, Integer>();
+        HashMap<Character, Boolean> oneHM = new HashMap<Character, Boolean>();
+        HashMap<Character, Boolean> twoHM = new HashMap<Character, Boolean>();
         for (char c : oneS.toCharArray()) {
-            if (oneHM.get(c) == null) oneHM.put(c, 1);
-            else oneHM.put(c, oneHM.get(c) + 1);
+            oneHM.put(c, true);
         }
         for (char c : twoS.toCharArray()) {
-            if (twoHM.get(c) == null) twoHM.put(c, 1);
-            else twoHM.put(c, twoHM.get(c) + 1);
+            twoHM.put(c, true);
         }
         return oneHM.equals(twoHM);
     }
